57f3e54262a4bbab090049691eb8737b926d822a
jemeric <jericmason@gmail.com>
2018-04-23 23:11:56 -0700

    Testing out command line readers

--- jemml-examples/ExampleRunner.cs
+++ jemml-examples/ExampleRunner.cs
@@ -1,14 +1,28 @@
 ﻿using System;
+using McMaster.Extensions.CommandLineUtils;
 
 namespace jemml_examples
 {
-    class ExampleRunner
+    public class ExampleRunner
     {
-        static void Main(string[] args)
+        public static void Main(string[] args) => CommandLineApplication.Execute<ExampleRunner>(args);
+
+        [Option(Description = "The input file", ShortName = "i", LongName = "inputFile")]
+        public string InputFile { get; }
+
+        [Option(Description = "The output file", ShortName = "o", LongName = "outputFile")]
+        public string OutputFile { get; }
+
+        private void OnExecute()
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Number of arguments: {0}", args.Length);
+            Console.WriteLine("input file {0}, output file {1}", InputFile, OutputFile);
             Console.ReadLine();
         }
+
+        //{
+        //    console.writeline("hello world!");
+        //    console.writeline("number of arguments: {0}", args.length);
+        //    console.readline();
+        //}
     }
 }
